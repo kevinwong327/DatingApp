ccd66183a052b7e4b9affe3fd6ec2c614c00ff0c
Kevin Wong <kevin.wong1@gmail.com>
2019-08-05 01:41:02 -0400

    End of Section 8 - Extending the API

---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.DTO;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, opt =>
+                {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
+                .ForMember(dest => dest.Age, opt =>
+                {
+                    opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+                });
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(dest => dest.PhotoUrl, opt =>
+                {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
+                .ForMember(dest => dest.Age, opt =>
+                {
+                    opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+                });
+            CreateMap<Photo, PhotosForDetailedDto>();
+        }
+
+        private void ForMember(Func<object, object> p1, Func<object, object> p2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
