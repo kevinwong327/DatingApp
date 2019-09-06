b6eee600a5c71a019c8d9de2f9132100c50840ea
Kevin Wong <kevin.wong1@gmail.com>
2019-09-06 11:40:08 -0400

    Sec14-144: Add additional filtering params to API

--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -13,6 +13,9 @@ namespace DatingApp.API.Helpers
 
         public int UserId { get; set; }
         public string Gender { get; set; }
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 99;
+
 
     }
 }
