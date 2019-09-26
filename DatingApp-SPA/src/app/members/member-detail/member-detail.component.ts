f43a1294dac4355de4e64df24d0c80e42770e002
Kevin Wong <kevin.wong1@gmail.com>
2019-09-26 16:19:24 -0400

    Sec 16-168: Add Query params to Angular route

--- DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from 'src/app/_models/user';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
 import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import { TabsetComponent } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-member-detail',
@@ -11,6 +12,9 @@ import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gal
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  //for Angular v8
+  //@ViewChild('memberTabs', {static: true}) memerTabs: TabsetComponent
+  @ViewChild('memberTabs') memberTabs: TabsetComponent
   user: User;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
@@ -23,6 +27,10 @@ export class MemberDetailComponent implements OnInit {
       this.user = data['user']
     });
 
+    this.route.queryParams.subscribe(params => {
+      const selectedTab = params['tab'];
+      this.memberTabs.tabs[selectedTab > 0 ? selectedTab : 0].active = true;
+    })
     this.galleryOptions = [
       {
         width: '500px',
@@ -48,12 +56,8 @@ export class MemberDetailComponent implements OnInit {
     return imageUrls;
   }
 
-  // loadUser() {
-  //   this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
-  //     this.user = user;
-  //   }, error => {
-  //     this.alertify.error(error);
-  //   })
-  // }
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
+  }
 
 }
